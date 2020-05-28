8ed81e5d8d1046334ace99bf169fc3aa1eb35041
mehdi asoodeh <asoodeh.dev@gmail.com>
2020-05-28 23:24:46 +0430

    added net receiver

---
+++ app/src/main/java/com/mahdi20/fullcodes/broadcastReceiverExample/net/NetworkReceiver.java
@@ -0,0 +1,36 @@
+package com.mahdi20.fullcodes.broadcastReceiverExample.net;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+
+public class NetworkReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            if (isOnline(context)) {
+                Log.e("keshav", "Online Connect Intenet ");
+            } else {
+                Log.e("keshav", "Conectivity Failure !!! ");
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isOnline(Context context) {
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            //should check null because in airplane mode it will be null
+            return (netInfo != null && netInfo.isConnected());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/mahdi20/fullcodes/serviceExample/BackgroundServiceExample.java
@@ -0,0 +1,4 @@
+package com.mahdi20.fullcodes.serviceExample;
+
+public class BackgroundServiceExample {
+}
